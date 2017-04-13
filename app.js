d3ec6bfbd96903d8bab3affebb0c1ba9df6126f9
Chetan Singh Soibam <chetansingh.soibam@lowes.com>
2017-04-13 11:39:21 +0530

    set up port

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var hi = 'hello';
-console.log(hi);
+// var hi = 'hello';
+// console.log(hi);
+
+var express = require('express');
+var app = express();
+
+var port = 5000;
+app.listen(port,function(err){
+  console.log('running on' + port);
+})
