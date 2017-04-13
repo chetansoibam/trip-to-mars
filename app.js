6d50a5cab18de93fe9dd53bf213bd9bd8c352fc9
Chetan Singh Soibam <chetansingh.soibam@lowes.com>
2017-04-13 12:05:08 +0530

    template set up

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var express = require('express');
 var app = express();
 
 var port = 5000;
+
+app.get('/',function(req , res){
+  res.send('Hello world');
+});
+
+app.get('/books',function(req , res){
+  res.send('Hello books');
+});
+
+
 app.listen(port,function(err){
   console.log('running on' + port);
-})
+});
